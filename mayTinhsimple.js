a2b21a9431c62617ce1ce08f0915bceb1d36283a
DO HOAI NAM <dhnam.2020@gmail.com>
2021-06-12 11:22:51 +0700

    mayTinhDonGian

---
+++ mayTinhsimple.js
@@ -0,0 +1,20 @@
+function cong(){
+    let a=+document.getElementById("sodautien").value;
+    let b=+document.getElementById("sothuhai").value;
+    let c = (a+b); alert(c);
+}
+function tru(){
+    let a=+document.getElementById("sodautien").value;
+    let b=+document.getElementById("sothuhai").value;
+    let c = (a-b); alert(c)
+}
+function nhan(){
+    let a=+document.getElementById("sodautien").value;
+    let b=+document.getElementById("sothuhai").value;
+    let c = (a*b); alert(c)
+}
+function chia(){
+    let a=+document.getElementById("sodautien").value;
+    let b=+document.getElementById("sothuhai").value;
+    let c = (a/b); alert(c)
+}
